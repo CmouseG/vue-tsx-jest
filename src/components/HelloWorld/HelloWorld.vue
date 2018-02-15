f1afbb54b4584f5b3359a90b10d8b6408488f87c
Guilherme Lima <guilhermelimak@gmail.com>
2018-02-15 08:40:56 -0200

    Initial commit

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  render () {
+    return <div class="hello">
+      <h1>{this.msg}</h1>
+    </div>
+  }
+}
+</script>
