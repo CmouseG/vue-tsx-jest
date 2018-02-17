c36efe1a0b6e9fc0a6c667098e1988daadf888cc
Guilherme Lima <guilhermelimak@gmail.com>
2018-02-17 05:47:29 -0200

    chore: Add jest

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,9 +5,11 @@ import { Container, MainInput } from './style'
 @Component
 class Home extends Vue {
   render(h: Function) {
-    return <Container>
-      <MainInput />
-    </Container>
+    return (
+      <Container>
+        <MainInput />
+      </Container>
+    )
   }
 }
 
