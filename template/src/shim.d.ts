c2938274754e0c16fe1ecb70948bcc0894a41095
Guilherme Lima <guilhermelimak@gmail.com>
2018-02-18 07:30:33 -0300

    feat: Add typechecking to component props

--- template/src/shim.d.ts
+++ template/src/shim.d.ts
@@ -1,5 +1,8 @@
 declare namespace JSX {
   interface Element {}
+  interface ElementAttributesProperty {
+    $props // Add type checking to component props
+  }
   interface IntrinsicElements {
     [elemName: string]: any
   }
